0e1650d2e0448bf3b6f4c0f3beff9842bbc2df2a
leila111111 <leila_merzak@outlook.fr>
2024-01-19 00:16:29 +0000

    tasks

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,14 +3,13 @@
 
 def minOperations(n):
     """task min operations"""
-    operations = 0
-    current_length = 1
+    num_operations = 0
+    length = 1
     while n > 1:
         for i in range(2, n + 1):
             if n % i == 0:
-                operations += i
-                current_length *= i
+                num_operations += i
+                length *= i
                 n //= i
                 break
-    return operations
-
+    return num_operations
