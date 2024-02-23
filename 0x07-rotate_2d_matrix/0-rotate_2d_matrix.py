c62e85249578b6d54ac89f4e6515afdaf8c5758a
leila111111 <leila_merzak@outlook.fr>
2024-02-23 00:33:07 +0000

    tasks

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rotate 2D Matrix"""
+
+def rotate_2d_matrix(matrix):
+    """rotate a matrix"""
+    rotation = [list(row) for row in matrix]
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n - 1, -1, -1):
+            matrix[i][n - 1 - j] = rotation[j][i]
